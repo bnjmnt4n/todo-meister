536c606c1f754f98094e9e283654bd7d10708cbe
Benjamin Tan <demoneaux@gmail.com>
2020-12-24 15:43:33 +0800

    Add `shell.nix` to configure Ruby and Postgres
    The `shell.nix` file installs Ruby dependencies and sets up a local
    Postgres server, which is automatically started up upon `nix-shell`.
    
    To update the Gem dependencies after changes to the Gemfile, the
    following command can be run:
    
    ```sh
    nix-shell -p bundix --run 'bundix -l'
    ```

---
+++ backend/shell.nix
@@ -0,0 +1,35 @@
+with (import <nixpkgs> {});
+let
+  env = bundlerEnv {
+    name = "cvwo-bundler-env";
+    inherit ruby;
+    gemfile  = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset   = ./gemset.nix;
+  };
+in stdenv.mkDerivation {
+  name = "cvwo";
+  buildInputs = [
+    bundix
+    env
+    env.wrappedRuby
+    postgresql
+  ];
+  # Initialize a local PostgreSQL instance.
+  # Based on https://gist.github.com/sebastian-stephan/d2013204e62070e5a56bc4f5415559a2.
+  shellHook = ''
+    export PGDATA=$PWD/postgres_data
+    export PGHOST=$PWD/postgres
+    export LOG_PATH=$PWD/postgres/LOG
+    export PGDATABASE=postgres
+    export DATABASE_URL="postgresql:///postgres?host=$PGHOST"
+    if [ ! -d $PGHOST ]; then
+      mkdir -p $PGHOST
+    fi
+    if [ ! -d $PGDATA ]; then
+      echo 'Initializing postgresql database...'
+      initdb $PGDATA --auth=trust >/dev/null
+    fi
+    pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -c unix_socket_directories=$PGHOST"
+  '';
+}
