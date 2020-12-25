5f6bf0ba7236ece4ed1b647832c018cf5736da94
Benjamin Tan <demoneaux@gmail.com>
2020-12-26 00:37:01 +0800

    Add User model
    For now, the password is a column in the users table, but I will
    eventually switch to using Rails' `has_secure_password` instead, since
    storing passwords in a database is a bad practice.

---
+++ backend/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :email, :password, :name, presence: true
+
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # TODO: switch to `digest` and `has_secure_password` instead
+  validates :password, length: { minimum: 8 }
+  validates :name, length: { minimum: 2 }
+end
