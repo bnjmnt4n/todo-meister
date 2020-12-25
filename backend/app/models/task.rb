0cf1e5cda567bc91dace64a330cdf50aedbca559
Benjamin Tan <demoneaux@gmail.com>
2020-12-26 01:06:24 +0800

    Add Tag model

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :subtasks
+  has_and_belongs_to_many :tags
 
   validates :name, presence: true
   validates :completed, inclusion: [true, false]
